9b25370fc366f6eb81f99f849d10719be9fe9545
Niroasd <123634396+Niroasd@users.noreply.github.com>
2023-09-13 15:01:23 +0300

    Added another practice file from chapter 2

---
+++ ch02/src/sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int firstInput;
+	int secondInput;
+	int sum;
+	
+	cout << "Type the first number:";
+	cin >> firstInput;
+	
+	cout << "Type the second number:";
+	cin >> secondInput;
+	
+	sum = firstInput + secondInput;
+	cout << "Sum of the numbers is " << sum << endl;
+	
+}
